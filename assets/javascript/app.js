956cd3a8641f1630b7f93ec944a41916101592bd
Krishna <kaddala@salesforce.com>
2019-11-10 19:46:57 -0800

    adding JS logic for onclick and Firebase event for adding a child to the DB

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,74 @@ var firebaseConfig = {
   
   // Create a variable to reference the database.
   var database = firebase.database();
+//clear function after  adding the train to the train schedule
+function clearTrainOnAdd(){
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#trainFrequency").val("");
+    }
 
+  //adding train using the submit button
+  $("#add-employee-btn").on("click", function(event) {
+    event.preventDefault();
 
+    var trainName = $("#trainName").val().trim();
+  var trainDestination = $("#trainDestination").val().trim();
+  //using moment update THIS SECTION TO USE HH:mm - military time
+  var firstTrainTime = moment($("#firstTrainTime").val().trim(), "MM/DD/YYYY").format("X");
+  var trainFrequency = $("#trainFrequency").val().trim();
+
+  // adding an object to hold train data to store
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    firstTrain: firstTrainTime,
+    frequency: trainFrequency
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newEmp.firstTrain);
+  console.log(newTrain.frequency);
+
+  clearTrainOnAdd();
+  });
+
+  //adding to firebase as a child event and this adds a row to the HTML when a user adds a train
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
   
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().firstTrain;
+    var trainFrequency = childSnapshot.val().frequency;
+    
+    console.log(empName);
+    console.log(empRole);
+    console.log(empStart);
+    console.log(empRate);
+
+    var frequencyInMins = "";
+
+    var nextTrainArrival = "";
+
+    var minutesAwayForNextTrain = "";
+
+
+    var newTrainAdded = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(frequencyInMins),
+        $("<td>").text(nextTrainArrival),
+        $("<td>").text(minutesAwayForNextTrain),
+      );
+    
+      // Append the new row to the table
+      $("#trainSchedule > tbody").append(newTrainAdded);
+
+  });
