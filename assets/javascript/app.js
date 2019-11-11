bbf1e530dd8614c2cc33691e7788c6424dc0903a
Krishna <kaddala@salesforce.com>
2019-11-10 19:19:42 -0800

    adding firebase references to the app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,23 @@
 // look at class example for firebase
 // firebase.app.config
 //firebase.database
+
+//initialize firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAY6AYGDx-D7WF3a0W5fcs_aK91uCPfp6c",
+    authDomain: "train-scheduler-assignme-3cc0b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-assignme-3cc0b.firebaseio.com",
+    projectId: "train-scheduler-assignme-3cc0b",
+    storageBucket: "train-scheduler-assignme-3cc0b.appspot.com",
+    messagingSenderId: "474090265715",
+    appId: "1:474090265715:web:d2823fa7bb9f7605491e06",
+    measurementId: "G-1R09HW8CW6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Create a variable to reference the database.
+  var database = firebase.database();
+
+
+  
